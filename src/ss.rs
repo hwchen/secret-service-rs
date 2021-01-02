d7a2fc2f8fe517238ec8cb477a68bc683db321be
Walther Chen <walther.chen@gmail.com>
2021-01-02 12:53:38 -0500

    remove extraneous static

--- src/ss.rs
+++ src/ss.rs
@@ -8,13 +8,13 @@
 // Definitions for secret service interactions
 
 // DBus Name
-pub const SS_DBUS_NAME: &'static str = "org.freedesktop.secrets";
+pub const SS_DBUS_NAME: &str = "org.freedesktop.secrets";
 
 // Item Properties
-pub const SS_ITEM_LABEL: &'static str = "org.freedesktop.Secret.Item.Label";
-pub const SS_ITEM_ATTRIBUTES: &'static str = "org.freedesktop.Secret.Item.Attributes";
+pub const SS_ITEM_LABEL: &str = "org.freedesktop.Secret.Item.Label";
+pub const SS_ITEM_ATTRIBUTES: &str = "org.freedesktop.Secret.Item.Attributes";
 
 // Algorithm Names
-pub const ALGORITHM_PLAIN: &'static str = "plain";
-pub const ALGORITHM_DH: &'static str = "dh-ietf1024-sha256-aes128-cbc-pkcs7";
+pub const ALGORITHM_PLAIN: &str = "plain";
+pub const ALGORITHM_DH: &str = "dh-ietf1024-sha256-aes128-cbc-pkcs7";
 
