9a28c8bebc88cd607d821ec7b3fe55ad974777e1
ComplexSpaces <complexspacescode@gmail.com>
2025-02-13 00:43:00 -0600

    Remove file license header boilerplate
    AFAIK this is not a requirement and all of it was varying levels of stale

--- src/ss.rs
+++ src/ss.rs
@@ -1,11 +1,4 @@
-//Copyright 2022 secret-service-rs Developers
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
-// Definitions for secret service interactions
+//! Definitions for secret service interactions
 
 // DBus Name
 pub const SS_DBUS_NAME: &str = "org.freedesktop.secrets";
