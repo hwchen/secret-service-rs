aa69ecfda987553fc3728ae571667a2c3efdbd2b
ComplexSpaces <complexspacescode@gmail.com>
2025-02-13 00:43:00 -0600

    Remove file license header boilerplate
    AFAIK this is not a requirement and all of it was varying levels of stale

--- src/session.rs
+++ src/session.rs
@@ -1,10 +1,3 @@
-//Copyright 2022 secret-service-rs Developers
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 // key exchange and crypto for session:
 // 1. Before session negotiation (openSession), set private key and public key using DH method.
 // 2. In session negotiation, send public key.
